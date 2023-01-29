e790b701395a3afb6388875ec04b0381643c6b21
YoonSubLim <yslim37@naver.com>
2023-01-29 21:48:15 +0900

    [DAY9] JPA 활용1 : 회원 목록 조회 upto 7.3

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -41,4 +42,12 @@ public class MemberController {
         memberService.join(member);
         return "redirect:/"; // 첫번째 화면으로 넘어가게 된다.
     }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 }
